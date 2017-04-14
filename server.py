23295f93c148ec3c64786193c2ddc53ff1f040fb
jingdao <chenjingdao@wustl.edu>
2017-04-14 13:06:46 -0400

    implement POST to server

--- server.py
+++ server.py
@@ -2,20 +2,28 @@
 import SimpleHTTPServer
 import SocketServer
 import urlparse
+import random
+from cgi import parse_header, parse_multipart
 
 class MyRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 	def do_GET(self):
+		if self.path.startswith('/random'):
+			url = '/report.html?id=%s' % random.randint(1,62)
+			self.send_response(302,'')
+			self.send_header('Location',url)
+			self.end_headers()
 		if self.path.startswith('/report.html'):
 			f = open('report.html','r')
 			data = f.read()
 			query = urlparse.parse_qs(urlparse.urlparse(self.path).query)
-			original_id = 1
 			if 'id' in query:
 				try:
 					original_id = int(query['id'][0])
 				except ValueError:
 					pass
-			original_path = '/%s/image_%04d.jpg' % ('caffe_util/101_ObjectCategories/chair',original_id)
+				original_path = '/%s/image_%04d.jpg' % ('caffe_util/101_ObjectCategories/chair',original_id)
+			else:
+				original_path = '/default.jpg'
 			http_response = data % (original_path)
 
 			self.send_response(200,'')
@@ -25,6 +33,18 @@ class MyRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 			self.request.sendall(http_response)
 		else:	
 			SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+	
+	def do_POST(self):
+		ctype, pdict = parse_header(self.headers['content-type'])
+		if ctype == 'multipart/form-data':
+			postvars = parse_multipart(self.rfile, pdict)
+			data = postvars['myfile'][0]
+			f = open('default.jpg','wb')
+			f.write(data)
+			f.close()
+		self.send_response(302,'')
+		self.send_header('Location','/report.html')
+		self.end_headers()
 
 server = SocketServer.TCPServer(('0.0.0.0', 8888), MyRequestHandler)
 server.serve_forever()
